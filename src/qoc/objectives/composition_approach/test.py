f86cbdaa87b913a152fa9441c3338604e09bb24e
Veronika Kurth <veronika.kurth@posteo.de>
2026-04-24 16:31:10 +0900

    Test usage of Objective for gate synthesis

--- src/qoc/objectives/composition_approach/test.py
+++ src/qoc/objectives/composition_approach/test.py
@@ -1,13 +1,31 @@
-from qutip import basis
+import qutip as qt
 from base import Objective
 from state_transfer import state_transfer_fidelity
+from gate_synthesis import gate_synthesis_fidelity
+from qutip.qip.operations import rx
 
 # One possible way to construct Objective and compute how close the system's state is to the target
 # Requires the user to provide a function that does the computation. 
 # For us it might be difficult to validate it but it is flexible.
 if __name__ == '__main__':
-    current = basis(2, 0)
-    target = basis(2, 1)
+
+    # State transfer
+
+    current = qt.basis(2, 0)
+    target = qt.basis(2, 1)
     objective = Objective(target, state_transfer_fidelity) 
     fidelity = objective.compute(current)
+    print(fidelity)
+
+
+    # Gate synthesis
+    
+    # Ideal Hadamard gate (1 qubit)
+    H_ideal = qt.gates.hadamard_transform(1)
+    # Perturbed Hadamard: ideal + small random unitary rotation
+    theta = 0.000001
+    U_pert = H_ideal * rx(theta)
+    # Compute fidelity between two unitaries
+    objective_gate = Objective(H_ideal, gate_synthesis_fidelity)
+    gate_fidelity = objective_gate.compute(U_pert)
     print(fidelity)
