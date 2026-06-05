3d154c578fd74e39aa75a155d8121cd96db27532
Veronika Kurth <veronika.kurth@posteo.de>
2026-06-05 11:34:12 +0900

    WIP: Scipy optimizer wrappers

--- src/qoc/optimizers/base.py
+++ src/qoc/optimizers/base.py
@@ -1,10 +1,20 @@
 from abc import ABC, abstractmethod
 
+import scipy
+
 class OptimizerResult:
-    pass
+    # Any optimizer specific diagnostics
+    def __init__(self, **kwargs):
+        pass
 
 class Optimizer(ABC):
 
     @abstractmethod
-    def minimize(self, **kwargs) -> OptimizerResult:
+    def minimize(self, loss_and_grad: callable, x0, max_iter, tol, **kwargs) -> OptimizerResult:
         pass
+
+
+class ScipyLBFGS(Optimizer):
+
+    def minimize(self):
+        return scipy.optimize.minimize(method="L-BFGS-B")
