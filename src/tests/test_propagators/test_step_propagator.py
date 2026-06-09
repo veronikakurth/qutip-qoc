d2d51605829ed0d11dbb917fc2b232165504c037
Veronika Kurth <veronika.kurth@posteo.de>
2026-06-09 10:53:29 +0900

    Add composition test

--- src/tests/test_propagators/test_step_propagator.py
+++ src/tests/test_propagators/test_step_propagator.py
@@ -12,9 +12,8 @@ def test_propagator():
 
 
 @pytest.fixture
-def test_zero_closed_system():
+def test_closed_system():
     return ClosedSystem(H0=0 * sigmax(), H_controls=[0 * sigmay(), 0 * sigmaz()])
-
 @pytest.fixture
 def test_end_time():
     return 10
@@ -22,7 +21,7 @@ def test_end_time():
 
 @pytest.fixture
 def test_n_slices():
-    return 10
+    return 1
 
 
 @pytest.fixture
@@ -38,24 +37,53 @@ def test_times(test_end_time, test_n_slices):
 def test_control_amplitudes(test_times):
     return np.array([[np.pi for t in test_times] for i in range(2)])
 
+@pytest.fixture
+def test_control_amplitudes_generic(test_times):
+    return np.array([[1.0 for t in test_times] for i in range(2)])
 
+# TODO: once StepPropagator contains propagation function for a single slice, refactor tests - don't use indexing of the lsit
 class TestStepPropagatorCompute:
 
     def test_zero_Hamiltonian(
-        self, test_propagator, test_zero_closed_system, test_control_amplitudes, test_n_slices, test_dt
+        self, test_propagator, test_closed_system, test_control_amplitudes, test_n_slices, test_dt
     ):
         # Output must be identity
         result = test_propagator.compute(
-            test_zero_closed_system, test_n_slices, test_control_amplitudes, test_dt
-        )[-1]
-        assert np.allclose(result, np.identity(2))
+            test_closed_system, test_n_slices, test_control_amplitudes, test_dt
+        )[0]
+        rows_n, _ = test_closed_system.shape
+        assert np.allclose(result, np.identity(n=rows_n))
 
-    def test_unitarity(self):
+    def test_unitarity(
+        self, test_propagator, test_closed_system, test_control_amplitudes, test_n_slices, test_dt
+    ):
+        result = test_propagator.compute(
+            test_closed_system, test_n_slices, test_control_amplitudes, test_dt
+        )[0]
+
+        rows_n, _ = test_closed_system.shape
+        assert np.allclose(result @ result.conj().T, np.identity(rows_n))
+
+    def test_half_rotation(self):
         pass
 
-    def test_half_angle(self):
+    def test_full_X_rotation(self):
         pass
 
-    def test_composition(self):
-        # Test that matrix exponential is consistent
-        pass
+    def test_composition(self, test_propagator, test_closed_system, test_n_slices, test_control_amplitudes_generic, test_dt):
+        # U(u, 2dt) = U(u, dt)²
+        # Test that matrix exponential is self-consistent,
+        # i.e., that matrix product of two single-slice propagators equals
+        # the two-step propagator
+        res_single_dt = test_propagator.compute(
+            test_closed_system, test_n_slices, test_control_amplitudes_generic, test_dt
+        )[0]
+
+        res_double_dt = test_propagator.compute(
+            test_closed_system, test_n_slices, test_control_amplitudes_generic, 2*test_dt
+        )[0]
+
+        assert np.allclose(res_double_dt, (res_single_dt @ res_double_dt), atol=1e-14)
+
+
+
