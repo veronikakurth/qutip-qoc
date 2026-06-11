fde3b8359b1553004f0630f40d0e81d89bb17aec
Veronika Kurth <veronika.kurth@posteo.de>
2026-06-11 16:12:53 +0900

    Correct signature of Optimizer.minimize to align better with scipy.optimize.minimize; change OptimizerResult to data class

--- src/qoc/optimizers/base.py
+++ src/qoc/optimizers/base.py
@@ -1,21 +1,22 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
 import scipy
 
+@dataclass
 class OptimizerResult:
     # Any optimizer specific diagnostics
-    def __init__(self, **kwargs):
-        pass
+    n_iters: int
 
 class Optimizer(ABC):
 
     @abstractmethod
-    def minimize(self, loss_and_grad: callable, x0, max_iter, tol, **kwargs) -> OptimizerResult:
+    def minimize(self, fun: callable, x0, max_iter, tol, **kwargs) -> OptimizerResult:
         pass
 
 
 class ScipyLBFGS(Optimizer):
-
+    # TODO: add information about interface requirements for `fun` argument or link to the SciPy's documentation.
     def minimize(self, fun, x0, max_iter, tol, **kwargs):
         opt_options = {"maxiter": max_iter, "gtol": tol}
         return scipy.optimize.minimize(fun=fun, x0=x0.flatten(), jac=True, method="L-BFGS-B", options = (opt_options | kwargs))
