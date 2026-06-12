Correct error in gradient calculation; make sure dt is consistent everywhere; correctness test passes for single qubit

src/qoc/solvers/grape.py
@@ -34,7 +34,7 @@ class GRAPE(OCPSolver):
 
         times = problem.times
 
-        dt = times[-1]/len(times)
+        dt = times[1] - times[0]
 
         u0 = problem.initial_pulses # (K, N)
         K, N = u0.shape
@@ -50,13 +50,14 @@ class GRAPE(OCPSolver):
 
             loss = objective.loss(Qobj(forward_evolution[-1]))
             grads = self._gradient(forward_evolution, co_states, system.H_controls, N, dt)
+            print(f"loss={loss:.6f}  fidelity={1-loss:.6f}")
 
             return loss, grads.ravel()
         
         opt_result = self.optimizer.minimize(loss_and_grad, x0=u0, max_iter=self.max_iter, tol=self.tol)
         
         if not opt_result.success:
-            print(f"Warning: optimiser {optimizer} used in GRAPE did not converge. \n Detailed optimiser report: {opt_result}")
+            print(f"Warning: optimiser {self.optimizer} used in GRAPE did not converge. \n Detailed optimiser report: {opt_result}")
 
         # return opt_result
         return Result(optimized_pulses=opt_result.x, fidelity=1 - opt_result.fun, n_iters=opt_result.nit, optimizer_info=opt_result)
@@ -105,7 +106,7 @@ class GRAPE(OCPSolver):
             co_state_jp1 = co_states[j + 1]
             for k, Hc in enumerate(H_c_arrays):
                 inner = (adj(co_state_jp1) @ Hc @ psi_j).item()
-                grads[k, j] = 2.0 * dt * np.real(np.conj(c) * inner)
+                grads[k, j] = -2.0 * dt * np.imag(np.conj(c) * inner)
         return grads
 
 def define_problem():

src/tests/test_integration/test_grape.py
@@ -1,36 +1,35 @@
 import pytest
 import numpy as np
 import qutip
-from qoc.solvers.grape import GRAPE
+from qoc.solvers.grape import GRAPE, adj
 from qoc.systems.controlled_system import ControlledSystem
 from qoc.problem import OptimalControlProblem
 from qoc.objectives import StateTransfer
+from qoc.dynamics.propagator import StepPropagator
 
 @pytest.fixture
 def x_closed_system():
     return ControlledSystem('closed', H0=0 * qutip.sigmax(), H_controls=[qutip.sigmax()])
-
-def check_pulse_area():
-    pass
 
 def test_state_transfer_single_qubit(x_closed_system):
     # Testing GRAPE for a state transfer on a single qubit for which
     # an analytical solution is known
     system = x_closed_system
     T = 10 
-    N = 100
+    N = 10
     dt = T / N
-    times = np.linspace(0, T, N)
-    initial_pulse = np.random.uniform(0, 0.0001, (system.dynamics.n_controls, N))
+    times = np.linspace(0, T, N, endpoint=False)
+    np.random.seed(0)
+
+    initial_pulse = np.random.uniform(-0.1, 0.1, (system.dynamics.n_controls, N))
     initial_state = qutip.basis(2, 0)
     target_state = qutip.basis(2, 1)
-
-    objective = StateTransfer(initial_state, target_state) # state fidelity will be used as performance measure
+    # By default, state fidelity is used as a performance measure in a state transfer task
+    objective = StateTransfer(initial_state, target_state)
     control_problem = OptimalControlProblem(system, objective, times, initial_pulse)
     algorithm = GRAPE()
     result = algorithm.solve(control_problem)
-    assert np.allclose(result.fidelity, 1.0 - 1e-4)
-    correct_pulse_values = np.array([np.pi / (2 * T)] * N)
-    assert np.allclose(result.optimized_pulses, correct_pulse_values)
-    
-
+    assert result.fidelity > 1.0 - 1e-4
+    pulse_area = np.sum(result.optimized_pulses) * dt
+    reduced = (pulse_area + np.pi) % (2*np.pi) - np.pi # reduce to (-pi, pi]
+    assert np.isclose(abs(reduced), np.pi/2, atol=1e-2)