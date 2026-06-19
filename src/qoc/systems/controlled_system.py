e7c33da8788e216618ff64e4a8baf3ed6d61d240
Veronika Kurth <veronika.kurth@posteo.de>
2026-06-19 16:54:35 +0900

    Improving interface of ControlledSystem

--- src/qoc/systems/controlled_system.py
+++ src/qoc/systems/controlled_system.py
@@ -1,12 +1,16 @@
 # Give possibility to also define custom dynamics in addition to choosing a type
 # For example, introduce a new argument, "system_model" which is None by default, but if provided with a correct type (System), it defines the dynamics of the controlled system
 from .closed import ClosedSystem
+from .open import OpenSystem
+from qutip import Qobj
+from typing import Literal
 
 # Facade class
 class ControlledSystem:
-    
-    def __init__(self, kind="closed", **system_init_params):
-        self.dynamics = SystemFactory.create(kind, system_init_params)
+    # TODO: the constructor is not user-friendly at the moment: no type hinting for system parameters. Shall we at least add controllable/non-controllable part of dynamics
+    # TODO: how to give hints for 'kind'?
+    def __init__(self, kind: Literal["closed", "open"], H0: Qobj, H_controls: list[Qobj], **system_params):
+        self.dynamics = SystemFactory.create(kind, {"H0": H0, "H_controls": H_controls} | system_params)
 
 # Factory class: decides on exact implementation of System based on "kind" parameter
 # Alternatively, it could also decide based on passed parameters
@@ -14,8 +18,9 @@ class SystemFactory:
 
     registry = {
         "closed": ClosedSystem,
+        "open": OpenSystem
     }
 
-    def create(kind, params):
+    def create(kind: Literal["closed", "open"], params: dict):
         # Return instantiated System object
         return SystemFactory.registry.get(kind)(**params)
