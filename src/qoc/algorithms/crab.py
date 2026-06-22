821ded43191f170bd8347be4fa7437bc1fb16145
Veronika Kurth <veronika.kurth@posteo.de>
2026-06-22 17:20:41 +0900

    A stub for CRAB with a possibility to use custom solvers

--- src/qoc/algorithms/crab.py
+++ src/qoc/algorithms/crab.py
@@ -1,8 +1,17 @@
-from .base import Algorithm
+from qoc.algorithms.base import Algorithm
+from qoc.dynamics.simulator import Simulator, default_simulator_for
 from qoc.problem import OptimalControlProblem
-from qoc.result import Result
+from qoc.algorithms.result import Result
+
 
 class CRAB(Algorithm):
 
+    def __init__(self, simulator: Simulator | None = None):
+        self.simulator = simulator
+
     def solve(self, problem: OptimalControlProblem) -> Result:
-        pass
+        simulator = self.simulator or default_simulator_for(problem.system.dynamics)
+        # TODO: parameterization + optimizer loop. Loss evaluation will call:
+        #   simulator.evolve(problem.system.dynamics, u, problem.times,
+        #                    problem.objective.initial)
+        raise NotImplementedError
