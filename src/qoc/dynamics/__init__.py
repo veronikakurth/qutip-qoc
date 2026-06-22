bccd5a80ac0ab184f4f2a7bc98535e615b8f9635
Veronika Kurth <veronika.kurth@posteo.de>
2026-06-22 17:18:18 +0900

    High-level imports of Simulators

---
+++ src/qoc/dynamics/__init__.py
@@ -0,0 +1,15 @@
+from .simulator import (
+    EvolutionResult,
+    MESolveSim,
+    SESolveSim,
+    Simulator,
+    default_simulator_for,
+)
+
+__all__ = [
+    "EvolutionResult",
+    "MESolveSim",
+    "SESolveSim",
+    "Simulator",
+    "default_simulator_for",
+]
