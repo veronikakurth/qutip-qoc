c65bcd84a8ae77022c14d1152b66281eb05a9ff4
Veronika Kurth <veronika.kurth@posteo.de>
2026-06-24 11:19:48 +0900

    Use Simulator in CRAB Algorithm

--- src/qoc/algorithms/crab.py
+++ src/qoc/algorithms/crab.py
@@ -1,5 +1,5 @@
 from qoc.algorithms.base import Algorithm
-from qoc.dynamics.simulator import Simulator, default_simulator_for
+from qoc.dynamics.simulator import Simulator
 from qoc.problem import OptimalControlProblem
 from qoc.algorithms.result import Result
 
@@ -7,10 +7,10 @@ from qoc.algorithms.result import Result
 class CRAB(Algorithm):
 
     def __init__(self, simulator: Simulator | None = None):
-        self.simulator = simulator
+        super().__init__(simulator)
 
     def solve(self, problem: OptimalControlProblem) -> Result:
-        simulator = self.simulator or default_simulator_for(problem.system.dynamics)
+        simulator = self._get_simulator(problem.system.dynamics)
         # TODO: parameterization + optimizer loop. Loss evaluation will call:
         #   simulator.evolve(problem.system.dynamics, u, problem.times,
         #                    problem.objective.initial)
