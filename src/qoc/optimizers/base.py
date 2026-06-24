2e32f37b85ca9b3f9e970d3238315717adf2e6be
Veronika Kurth <veronika.kurth@posteo.de>
2026-06-24 14:22:01 +0900

    Adapt Scipy optimizer wrapper to take bounds on a higher level than 'options'

--- src/qoc/optimizers/base.py
+++ src/qoc/optimizers/base.py
@@ -3,20 +3,37 @@ from dataclasses import dataclass
 
 import scipy
 
+
 @dataclass
 class OptimizerResult:
-    # Any optimizer specific diagnostics
+    # Any optimizer-specific diagnostics
     n_iters: int
+
 
 class Optimizer(ABC):
 
     @abstractmethod
-    def minimize(self, fun: callable, x0, max_iter, tol, **kwargs) -> OptimizerResult:
+    def minimize(
+        self,
+        fun: callable,
+        x0,
+        max_iter,
+        tol,
+        bounds: list[tuple[float | None, float | None]] | None = None,
+        **kwargs,
+    ) -> OptimizerResult:
         pass
 
 
 class ScipyLBFGS(Optimizer):
     # TODO: add information about interface requirements for `fun` argument or link to the SciPy's documentation.
-    def minimize(self, fun, x0, max_iter, tol, **kwargs):
+    def minimize(self, fun, x0, max_iter, tol, bounds=None, **kwargs):
         opt_options = {"maxiter": max_iter, "gtol": tol}
-        return scipy.optimize.minimize(fun=fun, x0=x0.flatten(), jac=True, method="L-BFGS-B", options = (opt_options | kwargs))
+        return scipy.optimize.minimize(
+            fun=fun,
+            x0=x0.flatten(),
+            jac=True,
+            method="L-BFGS-B",
+            bounds=bounds,
+            options=(opt_options | kwargs),
+        )
