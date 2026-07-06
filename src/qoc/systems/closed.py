287676e7d0ee61c31e433b8e626633bd2d8ef2fd
Veronika Kurth <veronika.kurth@posteo.de>
2026-07-06 16:29:13 +0900

    Call superclass constructor; remove properties already defined in base; rename build_hamiltonian -> build_generator; move validation to base class

--- src/qoc/systems/closed.py
+++ src/qoc/systems/closed.py
@@ -23,46 +23,19 @@ class ClosedSystem(System):
         H_controls : list[Qobj]
             Control Hamiltonians.
         """
-        _validate_hamiltonians(H0, H_controls)
-        self.H0 = H0
-        self.H_controls = H_controls
-
-    @property
-    def n_controls(self) -> int:
-        return len(self.H_controls)
-
-    @property
-    def dims(self) -> int: # TODO: it's another type from qutip.
-        return self.H0.dims
-
-    @property
-    def shape(self):
-        return self.H0.shape
+        super().__init__(H0, H_controls)
 
     # TODO: Would it be better to use Coefficient class for expressing time dependency in coefficients
-    def build_hamiltonian(
+    def build_generator(
         self, control_amplitudes: np.ndarray
     ) -> list:
         """Build the QuTiP time-dependent Hamiltonian list."""
-        H = [self.H0]
-        for k, H_k in enumerate(self.H_controls):
+        H = [self._H0]
+        for k, H_k in enumerate(self._H_controls):
             H.append([H_k, control_amplitudes[k]])
         return H
 
-    def build_hamiltonian_time_j(self, control_amplitudes: np.ndarray, j: int) -> Qobj:
-        H = self.H0 + sum(control_amplitudes[k][j] * self.H_controls[k] for k in range(self.n_controls))
+    # Currently, only used in GRAPE
+    def build_generator_time_j(self, control_amplitudes: np.ndarray, j: int) -> Qobj:
+        H = self._H0 + sum(control_amplitudes[k][j] * self._H_controls[k] for k in range(self.n_controls))
         return H
-
-
-
-def _validate_hamiltonians(H0: Qobj, H_controls: list) -> None: # TODO: I believe there may be more properties we need to check in order to propagate the system. Or are these done by the solver?
-    if not isinstance(H0, Qobj) or not H0.isoper:
-        raise TypeError("H0 must be a square operator Qobj")
-
-    for k, H_k in enumerate(H_controls):
-        if not isinstance(H_k, Qobj) or not H_k.isoper:
-            raise TypeError(f"H_controls[{k}] must be a square operator Qobj")
-        if H_k.dims != H0.dims:
-            raise ValueError(
-                f"H_controls[{k}] has dims {H_k.dims}, expected {H0.dims}"
-            )
