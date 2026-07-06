636cb98325d07dd0d34ee96e366cae8792f65603
Veronika Kurth <veronika.kurth@posteo.de>
2026-07-06 16:30:32 +0900

    Breathe life into OpenSystem: proper inheritance from base class; build_generator constructs Liouvillian; (for now very) basic c_ops validation

--- src/qoc/systems/open.py
+++ src/qoc/systems/open.py
@@ -1,7 +1,9 @@
 import numpy as np
-from qutip import Qobj
+from qutip import Qobj, QobjEvo, liouvillian
 
 from .base import System
+
+from qutip.typing import QobjEvoLike
 
 class OpenSystem(System):
     """
@@ -9,31 +11,44 @@ class OpenSystem(System):
 
     H(t) = H0 + sum_k u_k(t) * H_k, with Lindblad dissipators c_ops.
     """
-
-    def __init__(self, H0: Qobj, H_controls: list[Qobj], c_ops: list[Qobj]):
-        _validate_hamiltonians(H0, H_controls)
-        self.H0 = H0
-        self.H_controls = H_controls
-        self.c_ops = c_ops
+    # TODO: allow user to pass Liouvillian as in mesolve. There, H can be Liouvillian
+    # TODO: for smooth integration with mesolve:  
+    def __init__(self, H0: Qobj, H_controls: list[Qobj], c_ops: QobjEvoLike | list[QobjEvoLike]):
+        """
+        Parameters
+        ----------
+        H0 : Qobj
+            Drift Hamiltonian.
+        H_controls : list[Qobj]
+            Control Hamiltonians.
+        c_ops: QobjEvoLike | list[QobjEvoLike]
+            Collapse operators defining dissipation into the environment.
+        """
+        super().__init__(H0, H_controls)
+        self._validate_c_ops(c_ops)
+        self._c_ops = c_ops or []
 
     @property
-    def n_controls(self) -> int:
-        return len(self.H_controls)
+    def c_ops(self):
+        return self._c_ops
 
-    def build_hamiltonian(self, control_amplitudes: np.ndarray) -> list:
-        H = [self.H0]
-        for k, H_k in enumerate(self.H_controls):
+    # The output here can be a full Hamiltonian or Liouvillian - see how the latter is assembled in MESolver
+    def build_generator(self, control_amplitudes: np.ndarray) -> list:
+        H = [self._H0]
+        for k, H_k in enumerate(self._H_controls):
             H.append([H_k, control_amplitudes[k]])
         return H
+    
+    # Currently, only used in GRAPE
+    # TODO: Would it generally work with other parameterisations? And is it needed?
+    def build_generator_time_j(self, control_amplitudes: np.ndarray, j: int) -> Qobj:
+        H = self._H0 + sum(control_amplitudes[k][j] * self.H_controls[k] for k in range(self.n_controls))
+        L_j = liouvillian(H, self._c_ops)
+        return L_j
 
-def _validate_hamiltonians(H0: Qobj, H_controls: list) -> None:
-    if not isinstance(H0, Qobj) or not H0.isoper:
-        raise TypeError("H0 must be a square operator Qobj")
-
-    for k, H_k in enumerate(H_controls):
-        if not isinstance(H_k, Qobj) or not H_k.isoper:
-            raise TypeError(f"H_controls[{k}] must be a square operator Qobj")
-        if H_k.dims != H0.dims:
-            raise ValueError(
-                f"H_controls[{k}] has dims {H_k.dims}, expected {H0.dims}"
-            )
+    def _validate_c_ops(self, c_ops):
+        c_ops = c_ops or []
+        c_ops = [c_ops] if isinstance(c_ops, (Qobj, QobjEvo)) else c_ops
+        for c_op in c_ops:
+            if not isinstance(c_op, (Qobj, QobjEvo)):
+                raise TypeError("All `c_ops` must be a Qobj or QobjEvo")
